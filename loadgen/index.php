5c71c67d7fc7a6381cb4934e12b9d31bb98bcb20
Jenlmbr <54675613+Jenlmbr@users.noreply.github.com>
2020-05-24 16:59:56 +0200

    Add files via upload

---
+++ loadgen/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Demo</title>
+  <meta name="description" content="Container demo">
+  <!-- <link rel="stylesheet" href="css/styles.css"> -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<body>
+  <h1>Container demo</h1>
+
+  <form id="numbers" method="GET" action="/numbers.php">
+    <label for="number_of_values">Number of values</label><br />
+    <input type="text" name="number_of_values" /><br />
+    <input type="submit" value="Get new values"/>
+    <input type="reset" value="Reset"/>
+  </form>
+
+  <br/>
+  <p id="checking" style="display:none;">Loading <span id="attempt_counter"></span> numbers...</p>
+  <p id="loading_time"></p>
+  <br/>
+
+  <div id="results">
+  </div>
+
+  <script src="js/script.js"></script>
+</body>
+</html>
